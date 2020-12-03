aa9463da04c522efde9ef60b14047bd21513126a
Jack Kufa <jack@kufa.io>
2020-12-03 17:38:04 -0600

    Work on built in filter, add song w/ no synth

--- built_in_filter.m
+++ built_in_filter.m
@@ -1,81 +1,34 @@
-% part a)
-[x,Fs]=audioread('classical.wav');
-m =  length(x);
-m1 = pow2(nextpow2(m));
-X = fft(x,m1);
-f = (0:m1-1)*(Fs/m1);
-power = abs(X).^2/m1;
+%% Read in the file
+clearvars;
+close all;
+[x,fs] = audioread('tyler.ogg');
 
-figure
-plot(f(1:floor(m1/2)),power(1:floor(m1/2)));
-title('Power Spectrum Components');
-subtitle('classical.wav');
-xlabel('Frequency (Hz)');
+%% Plot both audio channels
+N = size(x,1); % Determine total number of samples in audio file
+
+%% Plot power spectrum
+df = fs / N;
+w = (-(N/2):(N/2)-1)*df;
+y = fft(x(:,1), N) / N; % Normalize freq
+y2 = fftshift(y);
+figure;
+plot(w,abs(y2));
+title('Power Spectrum');
+subtitle('tyler.ogg');
 ylabel('Power');
-xlim([0 4000])
-ylim([0 16])
-saveas(gcf,'a.png')
-
-% part b)
-M = 60;
-Fc = 600;
-fc = Fc/Fs;
-n = 0:1:M;
-hw = 0.54-.46.*cos(2*n*pi/M); % Hamming window
-hs = 2*fc*sinc(2*fc*(n-(M/2)));
-hw = hs.*hw;
-HW = fft(hw,m1);
-HW_dB = 20*log10(abs(HW));
-HW_dB = HW_dB(1:m1/2);
-n1 = 0:1:m1/2-1;
-
-figure
-plot(n1/m1, HW_dB);
-title('Hamming Filter Magnitude (dB)');
-subtitle('classical.wav');
 xlabel('Normalized frequency');
-ylabel('dB');
-saveas(gcf,'b.png')
 
-% part c)
-Y=X.*(HW.');
-power = abs(Y).^2/m1;
+%% Create 11th order lowpass filter
+n = 11;
+initial_freq = 500 / (fs/2);
+[b,a] = butter(n, initial_freq, 'low');
 
-figure
-plot(f(1:floor(m1/2)),power(1:floor(m1/2)));
-title('Power Spectrum Components: Lowpass Filter');
-subtitle('classical.wav');
-xlabel('Frequency (Hz)');
-ylabel('Power');
-xlim([0 4000])
-saveas(gcf,'c.png')
+%% Filter the signal
+f_out = filter(b, a, x);
 
-% part d)
-w = 1-((n-(M/2))./(M-2)).^2;
-hs = 2*.5*sinc(2*.5*(n-M/2))-2*fc*sinc(2*fc*(n-(M/2)));
-hw=hs.*w;
-HW = fft(hw, m1);
-HW_dB = 20*log10(abs(HW));
-HW_dB = HW_dB(1:m1/2);
-n1 = 0:1:m1/2-1;
+%% Construct audioplayer object and play
+p = audioplayer(f_out, fs);
+p.play;
 
-figure
-plot(n1/m1, HW_dB);
-title('Welch Filter Magnitude (dB)');
-subtitle('classical.wav');
-xlabel('Normalized frequency');
-ylabel('dB');
-saveas(gcf,'d.png')
-
-% part e)
-Y=X.*(HW.');
-power = abs(Y).^2/m1;
-
-figure
-plot(f(1:floor(m1/2)),power(1:floor(m1/2)));
-title('Power Spectrum Components: Highpass Filter');
-subtitle('classical.wav');
-xlabel('Frequency (Hz)');
-ylabel('Power');
-xlim([0 4000])
-saveas(gcf,'e.png')
+%% write to file
+audiowrite('tyler_no_synth.ogg',f_out,fs)

dd4de93edd19bb37b705a14f8f537d7e78de7ebb
jkufa <32912221+jkufa@users.noreply.github.com>
2020-12-03 18:10:14 -0600

    Update built_in_filter.m
    Fix file

--- built_in_filter.m
+++ built_in_filter.m
@@ -1,81 +1,39 @@
-% part a)
-[x,Fs]=audioread('classical.wav');
-m =  length(x);
-m1 = pow2(nextpow2(m));
-X = fft(x,m1);
-f = (0:m1-1)*(Fs/m1);
-power = abs(X).^2/m1;
+%% Read in the file
+clearvars;
+close all;
+[x,fs] = audioread('tyler.ogg');
 
-figure
-plot(f(1:floor(m1/2)),power(1:floor(m1/2)));
-title('Power Spectrum Components');
-subtitle('classical.wav');
-xlabel('Frequency (Hz)');
-ylabel('Power');
-xlim([0 4000])
-ylim([0 16])
-saveas(gcf,'a.png')
+%% Play original file
+% pOrig = audioplayer(x,fs);
+% pOrig.play;
 
-% part b)
-M = 60;
-Fc = 600;
-fc = Fc/Fs;
-n = 0:1:M;
-hw = 0.54-.46.*cos(2*n*pi/M); % Hamming window
-hs = 2*fc*sinc(2*fc*(n-(M/2)));
-hw = hs.*hw;
-HW = fft(hw,m1);
-HW_dB = 20*log10(abs(HW));
-HW_dB = HW_dB(1:m1/2);
-n1 = 0:1:m1/2-1;
+%% Plot both audio channels
+N = size(x,1); % Determine total number of samples in audio file
+figure;
+subplot(2,1,1);
+stem(1:N, x(:,1));
+title('Left Channel');
+subplot(2,1,2);
+stem(1:N, x(:,2));
+title('Right Channel');
 
-figure
-plot(n1/m1, HW_dB);
-title('Hamming Filter Magnitude (dB)');
-subtitle('classical.wav');
-xlabel('Normalized frequency');
-ylabel('dB');
-saveas(gcf,'b.png')
+%% Plot the spectrum
+df = fs / N;
+w = (-(N/2):(N/2)-1)*df;
+y = fft(x(:,1), N) / N; % For normalizing, but not needed for our analysis
+y2 = fftshift(y);
+figure;
+plot(w,abs(y2));
 
-% part c)
-Y=X.*(HW.');
-power = abs(Y).^2/m1;
+%% Design a bandpass filter that filters out between 700 to 12000 Hz
+n = 7;
+beginFreq = 4500 / (fs/2);
+endFreq = 6500 / (fs/2);
+[b,a] = butter(n, [beginFreq, endFreq], 'bandpass');
 
-figure
-plot(f(1:floor(m1/2)),power(1:floor(m1/2)));
-title('Power Spectrum Components: Lowpass Filter');
-subtitle('classical.wav');
-xlabel('Frequency (Hz)');
-ylabel('Power');
-xlim([0 4000])
-saveas(gcf,'c.png')
+%% Filter the signal
+fOut = filter(b, a, x);
 
-% part d)
-w = 1-((n-(M/2))./(M-2)).^2;
-hs = 2*.5*sinc(2*.5*(n-M/2))-2*fc*sinc(2*fc*(n-(M/2)));
-hw=hs.*w;
-HW = fft(hw, m1);
-HW_dB = 20*log10(abs(HW));
-HW_dB = HW_dB(1:m1/2);
-n1 = 0:1:m1/2-1;
-
-figure
-plot(n1/m1, HW_dB);
-title('Welch Filter Magnitude (dB)');
-subtitle('classical.wav');
-xlabel('Normalized frequency');
-ylabel('dB');
-saveas(gcf,'d.png')
-
-% part e)
-Y=X.*(HW.');
-power = abs(Y).^2/m1;
-
-figure
-plot(f(1:floor(m1/2)),power(1:floor(m1/2)));
-title('Power Spectrum Components: Highpass Filter');
-subtitle('classical.wav');
-xlabel('Frequency (Hz)');
-ylabel('Power');
-xlim([0 4000])
-saveas(gcf,'e.png')
+%% Construct audioplayer object and play
+p = audioplayer(fOut, fs);
+p.play;
